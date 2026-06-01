c260c7bb142ddad89e51eaf31078146e86581d59
mithlesh <kamaldewangan367@gmail.com>
2026-06-01 23:26:23 +0530

    add analytics

--- src/main.jsx
+++ src/main.jsx
@@ -39,6 +39,7 @@ import StudentNotices from './pages/Student/StudentNotices.jsx'
 import StudentGallery from './pages/Student/StudentGallery.jsx'
 import StudentLive from './pages/Student/StudentLive.jsx'
 import { registerServiceWorker } from './pwa/registerSW.js'
+import { Analytics } from "@vercel/analytics/react"
 
 // Global listener to capture PWA install prompt before React loads
 if (typeof window !== 'undefined') {
@@ -108,6 +109,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <Analytics />
     </QueryClientProvider>
   </StrictMode>,
 )
