df7688abdb46285eb153ac2d062764861a8c8dd2
mithlesh <kamaldewangan367@gmail.com>
2026-06-01 15:24:11 +0530

    add logo in install

--- vite.config.js
+++ vite.config.js
@@ -26,6 +26,12 @@ export default defineConfig({
         icons: [
           {
             src: '/logo.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/logo.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any',
